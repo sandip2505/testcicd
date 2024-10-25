f00fb2d48d757e888f0086c1bdd8af35523285d5
Sandip Ganava <sandipganava1234@gmail.com>
2024-10-25 19:18:18 +0530

    remove config.yml file

--- db.php
+++ db.php
@@ -1,17 +1,14 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
+$username = "onlineshop";
 $password = "";
 $db = "onlineshop";
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+$con = mysqli_connect($servername, $username, $password, $db, 3306);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
-?>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include "header.php";
 include "body.php";
 include "newslettter.php";
 include "footer.php";
+
 ?>
 		
 		
